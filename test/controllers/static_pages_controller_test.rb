bc377015e52f5fa8fe06be13a55f59fd1ed96266
John P. Reardon <jp@jpreardon.com>
2019-05-03 14:15:36 +0000

    Add title tests.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_select 'h1', 'Welcome!'
+    assert_select 'title', "Float Plan"
   end
   
   test "should get float plans" do
     get static_pages_float_plans_url
     assert_response :success
+    assert_select 'title', "Float Plans | Float Plan"
   end
   
   test "should get new float plan" do
     get static_pages_new_float_plan_url
     assert_response :success
+    assert_select 'title', "New Float Plan | Float Plan"
   end
   
   test "should get fleet page" do
     get static_pages_fleet_url
     assert_response :success
+    assert_select 'title', "Fleet | Float Plan"
   end
   
   test "should get crew page" do
     get static_pages_crew_url
     assert_response :success
+    assert_select 'title', "Crew | Float Plan"
   end
 
 end
